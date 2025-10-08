de06a7efe03634af5ee71e5695efe77f0716b94c
Amal Nath K <168527868+Dev0-code@users.noreply.github.com>
2025-10-08 12:01:00 +0530

    Time: 4 ms (95.09%) | Memory: 64.6 MB (89.59%) - LeetSync

--- 326-power-of-three/power-of-three.js
+++ 326-power-of-three/power-of-three.js
@@ -3,7 +3,5 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    if (n < 1) return false
-    if (n === 1) return true
-    return isPowerOfThree(n / 3)
+    return n > 0 && 1162261467 % n === 0;
 };
